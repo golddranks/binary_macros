87b43d9e15bc02797d0abc1d83cb2272466f5f1c
Pyry Kontio <pyry.kontio@drasa.eu>
2017-02-28 17:06:57 +0200

    Migrating to procedural macros.

---
+++ binary_macros_impl/src/lib.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+extern crate proc_macro_hack;
+
+extern crate data_encoding;
+
+proc_macro_expr_impl! {
+    pub fn base64(input: &str) -> String {
+        let byte_vec = data_encoding::base64::decode(input.as_bytes()).expect("Parse error!");
+        format!("b[{:?}]", byte_vec)
+    }
+    pub fn base64url(input: &str) -> String {
+        let byte_vec = data_encoding::base64url::decode(input.as_bytes()).expect("Parse error!");
+        format!("b[{:?}]", byte_vec)
+    }
+}
