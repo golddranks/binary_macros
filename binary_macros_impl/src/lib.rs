eefb2b6bcd0da993036e606e1bcac43aac06e128
Pyry Kontio <pyry.kontio@drasa.eu>
2017-02-28 17:11:50 +0200

    WIP

--- binary_macros_impl/src/lib.rs
+++ binary_macros_impl/src/lib.rs
@@ -4,11 +4,11 @@ extern crate proc_macro_hack;
 extern crate data_encoding;
 
 proc_macro_expr_impl! {
-    pub fn base64(input: &str) -> String {
+    pub fn base64_impl(input: &str) -> String {
         let byte_vec = data_encoding::base64::decode(input.as_bytes()).expect("Parse error!");
         format!("b[{:?}]", byte_vec)
     }
-    pub fn base64url(input: &str) -> String {
+    pub fn base64url_impl(input: &str) -> String {
         let byte_vec = data_encoding::base64url::decode(input.as_bytes()).expect("Parse error!");
         format!("b[{:?}]", byte_vec)
     }
