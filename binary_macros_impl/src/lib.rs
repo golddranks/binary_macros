8fd02bac369b9887b229b6ea83b01985bbae3e4a
Lohann Paterno Coutinho Ferreira <lohanncoutinho@gmail.com>
2020-10-27 19:18:25 -0300

    Support path relative to cargo manifest dir

--- binary_macros_impl/src/lib.rs
+++ binary_macros_impl/src/lib.rs
@@ -5,6 +5,7 @@ extern crate dotenv;
 
 use std::str::FromStr;
 use std::io::Read;
+use std::path::PathBuf;
 use proc_macro_hack::proc_macro_hack;
 use proc_macro::TokenStream;
 
@@ -16,6 +17,17 @@ fn helper(input: TokenStream, decoder: data_encoding::Encoding) -> TokenStream
     let byte_vec = if input.starts_with("file:") {
 
         let mut file = std::fs::File::open(&input[5..]).expect("Error opening file");
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).expect("Error reading file");
+        decoder.decode(contents.trim().as_bytes()).expect("Parse error")
+
+    } else if input.starts_with("relative:") {
+
+        let cargo_toml_directory = std::env::var("CARGO_MANIFEST_DIR")
+            .expect("Cannot find manifest file");
+        let mut path: PathBuf = cargo_toml_directory.into();
+        path.push(&input[9..]);
+        let mut file = std::fs::File::open(path).expect("Error opening file");
         let mut contents = String::new();
         file.read_to_string(&mut contents).expect("Error reading file");
         decoder.decode(contents.trim().as_bytes()).expect("Parse error")
